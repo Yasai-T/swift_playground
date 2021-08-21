1476a7505de3b49e84bc30cb337885189cb64fd7
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-08-21 22:16:51 +0900

    hello

--- Landmarks/Shared/ContentView.swift
+++ Landmarks/Shared/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack(alignment: .leading) {
+      Text("Hello, world!")
+        .font(.title)
+      Text("xcode !!!")
+        .font(.title2)
+    }
   }
 }
-
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
