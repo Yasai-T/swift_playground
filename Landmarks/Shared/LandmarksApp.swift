2bcb59e3340bf94dedefbd4952d64ca6f726cbf9
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-08-21 15:27:32 +0900

    init

---
+++ Landmarks/Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by 緑川知輝 on 2021/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
