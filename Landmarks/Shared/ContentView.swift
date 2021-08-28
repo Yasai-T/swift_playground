813f5ef45d03e70d505dbdc0568f4f8ff7f6be03
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-08-28 21:37:14 +0900

    refactor

--- Landmarks/Shared/ContentView.swift
+++ Landmarks/Shared/ContentView.swift
@@ -19,32 +19,26 @@ struct ContentView: View {
         repeat {
           newIndex = Int.random(in: 0...2)
         } while index == newIndex
-        
-          switch newIndex {
-          case 0:
-              soundPlayer.kickPlay()
-          case 1:
-              soundPlayer.tamPlay()
-          case 2:
-              soundPlayer.snarePlay()
-          default:
-              return
-          }
+
+        switch newIndex {
+        case 0:
+          soundPlayer.kickPlay()
+        case 1:
+          soundPlayer.tamPlay()
+        case 2:
+          soundPlayer.snarePlay()
+        default:
+          return
+        }
         index = newIndex
       }) {
         switch index {
         case 0:
-          Image("animal_hitsuji_shiboubi")
-            .resizable()
-            .aspectRatio(contentMode: ContentMode.fit)
+          ImageView(imageName: "animal_hitsuji_shiboubi")
         case 1:
-          Image("animal_kiboshi_iwa_hyrax")
-            .resizable()
-            .aspectRatio(contentMode: ContentMode.fit)
+          ImageView(imageName: "animal_kiboshi_iwa_hyrax")
         case 2:
-          Image("animal_usaghi_netherland_dwarf")
-            .resizable()
-            .aspectRatio(contentMode: ContentMode.fit)
+          ImageView(imageName: "animal_usaghi_netherland_dwarf")
         default:
           Text("Error")
         }
