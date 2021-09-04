e6b227dc82f4400462b00637d942ce2be01eb67e
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-09-04 14:05:46 +0900

    init

---
+++ MapPP/Shared/MapPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPPApp.swift
+//  Shared
+//
+//  Created by 緑川知輝 on 2021/09/04.
+//
+
+import SwiftUI
+
+@main
+struct MapPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
