2c72c136efb2b3724ed7bac80961af2941622e29
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-09-04 14:10:07 +0900

    add: map view

---
+++ MapPP/Shared/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  MapView
+//
+//  Created by 緑川知輝 on 2021/09/04.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
