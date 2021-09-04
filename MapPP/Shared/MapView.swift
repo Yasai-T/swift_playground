7720aebfe4a31fde9e7377bf668d6441c56eda1c
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-09-04 14:49:46 +0900

    feat: show map

--- MapPP/Shared/MapView.swift
+++ MapPP/Shared/MapView.swift
@@ -9,17 +9,31 @@ import MapKit
 import SwiftUI
 
 struct MapView: UIViewRepresentable {
+  let searchKey: String
   func makeUIView(context: Context) -> some MKMapView {
     MKMapView()
   }
 
   func updateUIView(_ uiView: UIViewType, context: Context) {
+    let geocoder = CLGeocoder()
+
+    geocoder.geocodeAddressString(
+      searchKey,
+      completionHandler: { (placemarks, error) in
+        if let unwrapPlacemarks = placemarks,
+          let firstPlacemark = unwrapPlacemarks.first,
+          let location = firstPlacemark.location
+        {
+          let targetCoodinate = location.coordinate
+          print(targetCoodinate)
+        }
+      })
 
   }
 }
 
 struct MapView_Previews: PreviewProvider {
   static var previews: some View {
-    MapView()
+    MapView(searchKey: "東京タワー")
   }
 }
