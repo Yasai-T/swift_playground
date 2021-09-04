cac82dee9037e19bca841f38948c9e802bb6e864
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-09-04 14:17:44 +0900

    feat: show map view

--- MapPP/Shared/MapView.swift
+++ MapPP/Shared/MapView.swift
@@ -5,16 +5,21 @@
 //  Created by 緑川知輝 on 2021/09/04.
 //
 
+import MapKit
 import SwiftUI
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct MapView: UIViewRepresentable {
+  func makeUIView(context: Context) -> some MKMapView {
+    MKMapView()
+  }
+
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+
+  }
 }
 
 struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
+  static var previews: some View {
+    MapView()
+  }
 }
