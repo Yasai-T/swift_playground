dd71aad1bb6cd0e0f9c4275aa7b46c476c5de384
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-09-04 14:06:09 +0900

    chore: format

--- MapPP/Shared/MapPPApp.swift
+++ MapPP/Shared/MapPPApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct MapPPApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
